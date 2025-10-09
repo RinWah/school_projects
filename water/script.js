9f6c393199b14142d98a9525d48f759722d532d2
rin <rinnuo041@gmail.com>
2025-10-08 20:43:15 -0400

    it's cute and has a customizable timer option.

--- water/script.js
+++ water/script.js
@@ -1,1 +1,12 @@
 let reminder;
+document.getElementById("start_button").addEventListener("click", () => {
+    const interval = parseInt(document.getElementById("interval").value);
+    alert("reminders started!");
+    reminder = setInterval(() => {
+        alert("time to drink water!");
+    }, interval);
+    });
+document.getElementById("stop_button").addEventListener("click", () => {
+    clearInterval(reminder);
+    alert("reminderes stopped.");
+});
