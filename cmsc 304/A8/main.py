cce83c168819a9a3c76a939906cfbf29971a99cb
RinWah <133681513+RinWah@users.noreply.github.com>
2025-11-23 14:03:57 -0500

    worked on 304 assignment 8

--- cmsc 304/A8/main.py
+++ cmsc 304/A8/main.py
@@ -1,21 +1,23 @@
-import sys
+from enum import Enum, auto #import enum class that allows python enum properties, built in
+from dataclasses import dataclass #idk what this is tbh
 
-def main():
-    if len(sys.argv) != 3:
-        print("usage: python tokenizer.py <input> <output>")
-        sys.exit(0)
+class TokenType(Enum): #make enum stuff, but idk why all of them are auto
+    LEFT_PARENTHESIS = auto()
+    RIGHT_PARENTHESIS = auto()
+    LEFT_BRACKET = auto()
+    RIGHT_BRACKET = auto()
+    WHILE_KEYWORD = auto()
+    RETURN_KEYWORD = auto()
+    EQUAL = auto()
+    COMMA = auto()
+    EOL = auto()
+    VARTYPE = auto()
+    IDENTIFIER = auto()
+    BINOP = auto()
+    NUMBER = auto()
 
-    input_path = sys.argv[1]
-    output_path = sys.argv[2]
-
-    with open(input_path, "r") as f:
-        text = f.read()
-
-    # ... process text into tokens ...
-
-    with open(output_path, "w") as f:
-        for token, lexeme in tokens:
-            f.write(f"{token} {lexeme}\n")
-
-if __name__ == "__main__":
-    main()
+@dataclass
+class Lex: #is this like saying hey all my tokens are going to be token type and all my lexemes are going to be str? also what are
+# lexemes again? words? 
+    token: TokenType
+    lexeme: str
