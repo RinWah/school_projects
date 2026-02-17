780fee8fcc115b9c3df4a886dfee4fb47f3b870b
rinwah <rinnuo041@gmail.com>
2026-02-17 13:02:59 -0500

    scratch that. 425 junit testing will just have to come with time.

--- spring2026/425/junit experimenting/practice1/clamp.java
+++
@@ -1,8 +0,0 @@
-public class clamp {
-    public static int clamp(int x, int min, int max) {
-        if (min > max) throw new IllegalArgumentException("min > max");
-        if (x < min) return min;
-        if (x > max) return max;
-        return x;
-    }
-}
