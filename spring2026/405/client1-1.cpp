2ba83e74a4d821c713abc3fbc39fff26454f09da
rinwah <rinnuo041@gmail.com>
2026-02-17 14:56:19 -0500

    stated 437 pa2

--- spring2026/405/client1-1.cpp
+++ spring2026/405/client1-1.cpp
@@ -1,4 +1,4 @@
-#include <stdlib.h>
+//#include <stdlib.h>
 #include <cstdlib>
 //#include <glut.h>
 #include <iostream>
---
+++ spring2026/437/PA2/ngram.py
@@ -0,0 +1,16 @@
+# i need a program that has a method/function with params of n [# in ngram], m [number of sentences], file.txt [file that we are processing to get ngrams from]
+# method needs
+# convert to lowercase
+# separate OR remove punctuation
+# identify sentence endings, only . ! ?
+
+# avoid "end. once" bigrams
+
+def processor():
+    # read file
+    # convert to lowercase
+    # separate / remove punctuation
+    # regex pattern: r"\w+|
+    # identify sentences
+    # make bigrams
+    # generate sentences
