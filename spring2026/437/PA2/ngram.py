f80ef4fd34a764f41547e182e2c954f768e5cad5
rinwah <rinnuo041@gmail.com>
2026-02-19 10:20:05 -0500

    437 PA2

--- spring2026/437/PA2/ngram.py
+++ spring2026/437/PA2/ngram.py
@@ -3,14 +3,36 @@
 # convert to lowercase
 # separate OR remove punctuation
 # identify sentence endings, only . ! ?
+from sys import argv # for the lists
+import re # for regex
 
 # avoid "end. once" bigrams
 
+# needs file input within function, let's say it's called texts
 def processor():
     # read file
-    # convert to lowercase
+    document = open("demofile.txt")
+    # convert to lowercase, don't considerably know where this is to go yet.
+    document.lower()
+    read_in = string(argv[1:])
+    processor(document, read_in, texts)
+
+
     # separate / remove punctuation
-    # regex pattern: r"\w+|
+    # regex pattern: r"\w+|[.!?,]"
+
+    # do some fancy slicing to get rid of the punctuation and/or separate characters/tokens.
+    split(argv) # i don't remember the args T_T
+    re.findall(r"\w+|[.!?,]", argv)
+
+    # \w+ = matches one word characters
+    # | OR
+    # [.!?,] = punctuation
     # identify sentences
     # make bigrams
     # generate sentences
+
+def main():
+    # file read in process
+    
+    processor()
