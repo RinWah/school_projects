dd7594b81faf0d5578e1118445af3e7e5edbfafe
rinwah <rinnuo041@gmail.com>
2026-03-25 14:32:58 -0400

    pa4

--- spring2026/437/PA4/wsd-ml.py
+++ spring2026/437/PA4/wsd-ml.py
@@ -5,6 +5,15 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.tree import DecisionTreeClassifier 
 from sklearn.svm import LinearSVC
 
+# description 
+# bagf of words feature representation
+# each sentence is converted into vector where each dimension represents a unique word from training data.
+# value is word's frequency
+# only use words from 'line-train.txt' to build vocab.
+
+# compare your results to that of the most frequent sense baseline and the results from your programming assignment 3 decision list.
+# 
+
 # handle CL args
 train_file = sys.argv[1]
 test_file = sys.argv[2]
@@ -31,9 +40,9 @@ for inst_id, inst_body in train_instances:
 # features from training data
 vectorizer = CountVectorizer()
 # learn vocab & transform training data
-X_train = vectorizer.fit.transform(train_contexts)
+X_train = vectorizer.fit.transform(train_contexts) # only from train file
 # choose model
-if model_choice == "DecisionTree":
+if model_name == "DecisionTree":
     clf = DecisionTreeClassifier()
 elif model_name == "SVM":
     clf = LinearSVC()
@@ -45,4 +54,11 @@ with open(test_file, 'r') as f:
     test_content = f.read()
 test_instances = re.findall(r'<instance id="([^"]+)">(.*?)</instance>', test_content, re.S)
 for inst_id, inst_body in test_instances:
-    context_match = re.search(r'<context>(.*?)')
+    context_match = re.search(r'<context>(.*?)</context>', inst_body, re.S)
+    if context_match:
+        test_text = " ".join(re.findall(r'\b[a-z]+\b', context_match.group(1).lower()))
+        # transform using the training vocabulary only
+        X_test = vectorizer.transform([test_text]) # using vocab from line-train.txt
+        prediction = clf.predict(X_test)[0]
+        # output in the required format
+        print(f'<answer instance="{inst_id}" senseid="{prediction}"/>')
