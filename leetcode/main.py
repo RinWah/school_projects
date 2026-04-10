810b8c8c089b4484f07ef8ed69fcb399c7869493
rinwah <rinnuo041@gmail.com>
2026-04-10 15:03:44 -0400

    leetcode

--- leetcode/main.py
+++ leetcode/main.py
@@ -1,3 +1,22 @@
-class main():
-    counts = {"a": 1}
-    print(counts)
+class Solution(object):
+    def findTheDifference(self, s, t):
+        counts = {}
+        
+        # Part 1: Count letters in s
+        for char in s:
+            if char in counts:
+                counts[char] += 1
+            else:
+                counts[char] = 1
+        
+        # Part 2: Look at letters in t
+        for char in t:
+            # How do we check if 'char' is the extra letter?
+            # Hint: Either it's not in the dictionary, 
+            # OR its count has already hit 0.
+            
+            if ___ not in ___ or ___ == 0:
+                return char
+            else:
+                # Subtract 1 from the count so we know we "used" one
+                counts[char] -= 1
