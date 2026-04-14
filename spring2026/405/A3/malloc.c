02d862f13f224918a6b73ee2cc549b45b08d73d7
rinwah <rinnuo041@gmail.com>
2026-04-14 11:02:22 -0400

    405 fixes

--- spring2026/405/A3/malloc.c
+++ spring2026/405/A3/malloc.c
@@ -12,7 +12,17 @@ struct block_meta {
 #define META_SIZE sizeof(struct block_meta)
 void *global_base = NULL; 
 struct block_meta *get_block_ptr(void *ptr){
-    
+    return (struct block_meta*)ptr - 1;
+}
+void free(void *ptr) {
+    if(!ptr){
+        return;//no input, no output
+    }
+    //find metadata with pointer
+    struct block_meta* block_ptr=get_block_ptr(ptr);
+    //free block
+    block_ptr->free=1;
+    //merging logic to combine free memories
 }
 struct block_meta *find_best_fit(struct block_meta **last, size_t size) {
     struct block_meta *current = global_base;
@@ -36,7 +46,7 @@ struct block_meta *request_space(struct block_meta* last, size_t size){
     block=sbrk(0);
     void *request=sbrk(size + META_SIZE);
     //check if sbrk failed
-    f(request==(void*)-1){
+    if(request==(void*)-1){
         return NULL; //sbrk failed
     }
     //if theres a last block link them together
@@ -75,6 +85,23 @@ void *malloc(size_t size) {
         } else{ // find suitable free block to use
             block->free=0;
             //splitting logic
+            if((block->size)>=(size+META_SIZE+1)){
+                //create note for leftover piece
+                //new metadata starts after block data
+                struct block_meta *new_block=(struct block_meta*)((char *)(block_1)+size);
+                //setup new block's metadata
+                new_block->size=block->size-size-META_SIZE;
+                new_block->free=1;
+                new_block->next=block->next;
+                new_block->prev=block;//link back to preexisting block
+                //update preexisting block
+                block->size=size;
+                block->next=new_block;
+                //if theres a block after, update prev pointer
+                if(new_block->next){
+                    new_block->next->prev=new_block;
+                }
+            }
         }
     }
     //return block+1 to hide metadata from user
